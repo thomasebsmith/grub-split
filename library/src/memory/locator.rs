5ae89117da92fd5505dfd70d6185b3feb37837d1
Thomas Smith <thsm@umich.edu>
2022-09-20 23:59:02 -0400

    Document MemoryLocator and MemoryReader traits

--- library/src/memory/locator.rs
+++ library/src/memory/locator.rs
@@ -2,6 +2,14 @@ use std::io;
 
 use super::address::Address;
 
+/// Trait for types that can locate a library's starting address in memory.
 pub trait MemoryLocator {
+    /// Finds the address at which `library` begins in virtual memory in a
+    /// type-dependent context.
+    ///
+    /// Returns the library's starting address in virtual memory if it was
+    /// found.
+    ///
+    /// Returns an IO error if the library could not be found.
     fn locate(&mut self, library: &str) -> io::Result<Address>;
 }
--- library/src/memory/reader.rs
+++ library/src/memory/reader.rs
@@ -2,12 +2,28 @@ use std::io;
 
 use super::address::{AddressRange, VariableLengthAddressRange};
 
+/// Trait for types that can read byte sequences from memory.
 pub trait MemoryReader {
+    /// Copies the bytes present in `range` into a vector and returns them.
+    ///
+    /// The exact context for `range` is type-dependent.
+    ///
+    /// Returns an IO error if the bytes could not be read.
     fn read_vec(
         &mut self,
         range: VariableLengthAddressRange,
     ) -> io::Result<Vec<u8>>;
 
+    /// Copies the bytes present in `range` into a fixed-size array and returns
+    /// them.
+    ///
+    /// The exact context for `range` is type-dependent.
+    ///
+    /// Returns an IO error if the bytes could not be read.
+    ///
+    /// The default implementation uses [`read_vec`](MemoryReader::read_vec)
+    /// internally; implementations should override this if better performance
+    /// is attainable using another method.
     fn read<const NUM_BYTES: usize>(
         &mut self,
         range: AddressRange<NUM_BYTES>,
